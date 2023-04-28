0405f11177f49c28c4f390b86f57120bb65d75d6
Philip Sung <75019527+Philip-sung@users.noreply.github.com>
2023-04-28 11:28:39 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+    answer = accumulate(numbers.begin(), numbers.end(), (float)0) / numbers.size();
+    return answer;
+}
