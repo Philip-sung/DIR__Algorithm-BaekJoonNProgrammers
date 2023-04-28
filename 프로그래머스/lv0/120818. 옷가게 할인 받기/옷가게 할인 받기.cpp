ca712f98f4cd4e5294bf5186358037f6f9d92495
Philip Sung <75019527+Philip-sung@users.noreply.github.com>
2023-04-28 11:33:16 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.08 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    if (price >= 100000 && price < 300000)
+        price *= 0.95;
+    else if (price >= 300000 && price < 500000)
+        price *= 0.9;
+    else if (price >= 500000)
+        price *= 0.8;
+    
+    answer = price;
+    return answer;
+}
