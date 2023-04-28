1743a48475da879d9ae6101477cf18bb2e230e72
Philip Sung <75019527+Philip-sung@users.noreply.github.com>
2023-04-28 11:32:34 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> sides) {
+    int answer = 0;
+    int maxNum = 0;
+    int sum = 0;
+    
+    for (int i : sides){
+        if(maxNum < i)
+            maxNum = i;
+        sum += i;
+    }
+    
+    if (maxNum >= sum/2 + sum % 2)
+        answer = 2;
+    else if (maxNum < sum/2 + sum % 2)
+        answer = 1;
+    
+    return answer;
+}
