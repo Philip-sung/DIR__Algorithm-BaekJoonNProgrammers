9dcb491e1438b63de3bcf5355d1d1724db690ac8
Philip Sung <75019527+Philip-sung@users.noreply.github.com>
2023-05-24 13:46:40 +0900

    [unrated] Title: 귤 고르기, Time: 42.47 ms, Memory: 45.4 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
@@ -1,45 +1,21 @@
-#include <string>
 #include <vector>
-#include <unordered_map>
-#include <set>
-#include <iostream>
-
+#include <algorithm>
 using namespace std;
-
-int solution(int k, vector<int> tangerine) {
-    int answer = 0;
-    unordered_map<int,int> size_count;
-    vector<set<int>> count_size(1);    
-
-    for(int size : tangerine){
-        if(size_count.find(size) == size_count.end()){
-            size_count.insert(pair<int,int>(size,1));
-        }
-        else {
-            size_count.at(size)++;
-        }
-
-        int curCount = size_count.at(size);
-        if(count_size.size() - 1 < curCount){
-            count_size.push_back(set<int>());
-        }
-        count_size.at(curCount).insert(size);
-        if(curCount > 1){
-            count_size.at(curCount - 1).erase(size);
-        }
+int solution(int k, vector<int> tangerine)
+{
+    int t[10000001]={};
+    int i, c=0, p=0;
+    for(i=0; i<tangerine.size(); ++i)
+        t[tangerine[i]]++;
+    vector<int> v;
+    for(i=1; i<=10000000; ++i)
+        if(t[i])
+            v.push_back(t[i]);
+    sort(v.rbegin(), v.rend());
+    while(k>0)
+    {
+        k-=v[p++];
+        c++;
     }
-
-    for(int i = 0; i < count_size.size(); i++){
-        int curSizeBoxes = count_size.at(count_size.size() - 1 - i).size();
-        for(int j = 0; j < curSizeBoxes; j++){
-            answer++;
-            k = k - (count_size.size() - 1 - i);
-            if(k <= 0)
-                break;
-        }
-        if(k <= 0)
-            break;
-    }
-
-    return answer;
+    return c;
 }
