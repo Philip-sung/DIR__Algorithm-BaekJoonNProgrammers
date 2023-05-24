180718ee61b27aec0799052035d52bc09cb86357
Philip Sung <75019527+Philip-sung@users.noreply.github.com>
2023-05-24 13:45:27 +0900

    [unrated] Title: 귤 고르기, Time: 50.88 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
@@ -10,7 +10,7 @@ int solution(int k, vector<int> tangerine) {
     int answer = 0;
     unordered_map<int,int> size_count;
     vector<set<int>> count_size(1);    
-    
+
     for(int size : tangerine){
         if(size_count.find(size) == size_count.end()){
             size_count.insert(pair<int,int>(size,1));
@@ -18,7 +18,7 @@ int solution(int k, vector<int> tangerine) {
         else {
             size_count.at(size)++;
         }
-        
+
         int curCount = size_count.at(size);
         if(count_size.size() - 1 < curCount){
             count_size.push_back(set<int>());
@@ -28,7 +28,7 @@ int solution(int k, vector<int> tangerine) {
             count_size.at(curCount - 1).erase(size);
         }
     }
-    
+
     for(int i = 0; i < count_size.size(); i++){
         int curSizeBoxes = count_size.at(count_size.size() - 1 - i).size();
         for(int j = 0; j < curSizeBoxes; j++){
@@ -40,6 +40,6 @@ int solution(int k, vector<int> tangerine) {
         if(k <= 0)
             break;
     }
-    
+
     return answer;
 }
