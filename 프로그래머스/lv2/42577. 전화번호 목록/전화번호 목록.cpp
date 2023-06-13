6a1cd169d620da2a8589889e419dece32e8270db
Philip Sung <75019527+Philip-sung@users.noreply.github.com>
2023-06-13 13:57:44 +0900

    [level 2] Title: 전화번호 목록, Time: 1918.45 ms, Memory: 215 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,22 +1,25 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 #include <iostream>
+
 using namespace std;
 
-bool solution(vector<string> phoneBook) {
+bool solution(vector<string> phone_book) {
     bool answer = true;
-
-    sort(phoneBook.begin(), phoneBook.end());
-
-    for ( int i = 0 ; i < phoneBook.size() - 1 ; i++ )
-    {
-        if ( phoneBook[i] == phoneBook[i+1].substr(0, phoneBook[i].size()) )
-        {
-            answer = false;
-            break;
+    vector<unordered_set<string>> findMap(21);
+    for(string s : phone_book){
+        for(int i = 1; i < s.length(); i++){
+            findMap.at(i).insert(s.substr(0,i));
         }
     }
 
+    for(string str : phone_book){
+        int strlen = str.length();
+        if(findMap.at(strlen).find(str) != findMap.at(strlen).end()){
+            answer = false;
+        }
+
+    }
     return answer;
 }
